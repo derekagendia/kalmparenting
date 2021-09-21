d9cc11b69dc8666db7cf2b4b6bc13bcb66af7b4b
stevy Endaman <stevyabessolo@gmail.com>
2021-09-21 20:46:22 +0200

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
     public function index()
     {
-        return view('event.index');
+        $data = [
+            'events' => Event::orderBy('created_at', 'desc')->where('status','PUBLISHED')->paginate(6)
+        ];
+        return view('event.index',$data);
     }
 
     public function detail()
