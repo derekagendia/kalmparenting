da16e19023111304516b4949057597449474450c
stevy Endaman <stevyabessolo@gmail.com>
2021-09-21 20:46:26 +0200

    Update SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use TCG\Voyager\Models\Post;
 
 class SiteController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $data = [
+            'posts' => Post::orderBy('created_at', 'desc')->where('status','PUBLISHED')->paginate(3)
+        ];
+        return view('index',$data);
     }
 }
